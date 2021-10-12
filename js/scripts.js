444fecdb01b6fdfcc310c7fe8b4c0bd03bc24dc2
Slumrag <mrgreenthecristal@yandex.ru>
2021-10-12 12:26:04 +0300

    adaptivity and fancybox

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const schemeSvg=document.querySelector(".scheme-svg");
 const totalPriceTag=document.querySelector(".price-total");
+const menuButton=document.querySelector(".mobile-menu");
+const menu=document.querySelector(".menu");
 let cost=800;
 let totalPrice=0;
 let seatsSelected=0;
@@ -12,4 +14,7 @@ schemeSvg.addEventListener('click',(event)=>{
         totalPriceTag.textContent=totalPrice;
     }
     
+})
+menuButton.addEventListener('click', ()=>{
+   menu.classList.toggle("is-open");
 })
